475d39e722691130c3e4f98d50d7c189b475c0c6
afetism <aismayilova1215@gmail.com>
2024-02-21 19:30:24 +0400

    Just Part of Program

---
+++ Moduls/Bank.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankSistemproject.Moduls
+{
+    //Clients[]
+    //showCardBalance(BankCard)
+
+    //Clients[]
+    //showCardBalance(BankCard)
+    internal class Bank
+    {
+        static int id=1;
+
+        public Bank(string name)
+        {
+            Name=name;
+            Clients=new List<Client>();
+        }
+
+        public int Id { get; set; } = ++id;
+        public string Name { get; set; }
+        
+        public List<Client> Clients { get; set; }
+
+        void addClient(string name, string surname, int age,decimal balance, int cardYears)
+        {
+            Clients.Add(new Client(name, surname, age, salary, Name, balance, cardYears));
+        }
+        
+        void ShowClient()
+        {
+            foreach (Client client in Clients)
+                   Console.WriteLine(client.ToString());
+            
+        }
+
+    }
+}
